2941cc019f76efa76f863efd60d0d7baa1717d95
shiny-jerome <shiny.j22@gmail.com>
2025-08-07 18:25:50 +0530

    hc

--- GroceryApplication/src/test/java/testCase/Sample.java
+++ GroceryApplication/src/test/java/testCase/Sample.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -11,7 +12,7 @@ public class Sample {
   @Test
   @Parameters("browser")
   public void f(String browsername) {
-	  WebDriver driver=new ChromeDriver();
+	  WebDriver driver=new EdgeDriver();
 		 driver.get("https://www.google.com");
 		 driver.manage().window().maximize();
 		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
