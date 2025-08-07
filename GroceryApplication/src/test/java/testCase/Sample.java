a704aa1fc8a5bff265bd47edc88bf0dd61426776
shiny-jerome <shiny.j22@gmail.com>
2025-08-07 15:56:28 +0530

    commit

---
+++ GroceryApplication/src/test/java/testCase/Sample.java
@@ -0,0 +1,21 @@
+package testCase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Sample {
+  @Test
+  public void f() {
+	  WebDriver driver=new ChromeDriver();
+		 driver.get("https://www.google.com");
+		 driver.manage().window().maximize();
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
+		 
+		 System.out.println("Page source="+driver.getPageSource()); 
+		 System.out.println("Page title="+driver.getTitle()); 
+		 System.out.println("Current url="+driver.getCurrentUrl()); 
+  }
+}
